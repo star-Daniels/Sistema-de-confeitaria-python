85f036c9ae064f401d9e5b5c47a00a7179cb2f7b
Daniel Santos <166452209+star-Daniels@users.noreply.github.com>
2026-09-10 12:02:26 -0300

    listar vendas agora é uma pilha fila de prioridade adicionada

--- HISTORICO/historico.py
+++ HISTORICO/historico.py
@@ -1,11 +1,41 @@
+from ESTRUTURAS.pilha import Pilha
+
+pilha_vendas = Pilha()
+
+
 def listar_vendas(vendas):
-    id = 1
+
+    pilha_vendas.itens = []
+
     for venda in vendas:
-        
-        print(f"==========VENDA | {id}\n")
-        id += 1
+        pilha_vendas.empilhar(venda)
+
+    if pilha_vendas.vazia():
+        print("Nenhuma venda registrada.")
+        return
+
+    id = 1
+    vendas_temporarias = []
+
+    while not pilha_vendas.vazia(): #a venda temporaria é so pq se eu desempilhar  meio que vai ficar vazia depois ai eu guardo ela pra depois ainda ter mesmo depoi de desempilhar
+
+        venda = pilha_vendas.desempilhar()
+
+        print(f"========== VENDA | {id}\n")
+
         for item in venda.itens:
-            print(f"{item.bolo.nome} - {item.quantidade} | R$ {item.total}\n_________\n")
+            print(
+                f"{item.bolo.nome} - "
+                f"{item.quantidade} | "
+                f"R$ {item.total}\n"
+                "_________________\n"
+            )
+
+        vendas_temporarias.append(venda)
+        id += 1
+
+    for venda in vendas_temporarias:
+        pilha_vendas.empilhar(venda)
 
 def listar_vendas_por_valor(vendas):
 
